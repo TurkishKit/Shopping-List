d7321ee9879e46aee2e63925d28b5b4d63a9d830
Ege Sucu <iletisim@egesucu.com>
2019-06-26 16:28:07 +0300

    Initial Commit

---
+++ Shopping List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping List
+//
+//  Created by Ege Sucu on 26.06.2019.
+//  Copyright © 2019 Ege Sucu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
